aaaa2f6cf894b34f38963a5ca9b3290d27426d9d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-05-18 12:22:01 -0500

    [+] Component: Funcionalidad cambio estado contacto (Contacts)

--- src/components/containers/Contacts.jsx
+++ src/components/containers/Contacts.jsx
@@ -1,20 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { STATE } from '../../models/State.class';
 
 
 const Contacts = () => {
 
-    const [ contacts, setContacts ] = useState([
-        { name: 'Luisa', email: 'luisa@mail.co', state: true },
-        { name: 'Juliana', email: 'Juliana@mail.co', state: false },
-        { name: 'Elisa', email: 'elisa@mail.co', state: true }
-    ]);
+    const 
+        [ loading, setLoading ] = useState( true ),
+        [ contacts, setContacts ] = useState([
+            { name: 'Luisa', email: 'luisa@mail.co', state: STATE.CONECTED },
+            { name: 'Juliana', email: 'Juliana@mail.co', state: STATE.DISCONNECTED },
+            { name: 'Elisa', email: 'elisa@mail.co', state: STATE.CONECTED }
+        ]);
+
+    useEffect(() => {
+        console.log( 'Contact state has been modified!' );
+        setLoading( false );
+
+        return () => {
+            console.log( 'Contacts component is going to be unmount!' );
+        };
+    }, [ contacts ] );
+
+    const stateContact = ( task ) => {
+        console.log( 'Complete this task:', task );
+
+        const
+            index = contacts.indexOf( task ),
+            tempContacts = [ ...contacts ];
+
+        tempContacts[ index ].state = ! tempContacts[ index ].state;
+        setContacts( tempContacts );
+    }
 
     return (
-        <div class="list-group">
+        <div className="list-group">
             
             {   contacts.map( ( contact, index ) => (
-                    <a href="#" class="list-group-item list-group-item-action">
+                    <a href="#/" key={ index } className="list-group-item list-group-item-action">
+                        <i onClick={ () => stateContact( contact ) } className={ `bi ${ contact.state ? 'bi-toggle-on' : 'bi-toggle-off' }` }></i> 
                         { contact.name }
+                        
                     </a>
             ))}
             
