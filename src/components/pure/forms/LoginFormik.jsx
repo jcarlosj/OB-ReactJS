e5d212b676f420a5b6b2840f272cfdac772e1fa1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-05-20 15:35:00 -0500

    [+] Component: Cambia implementacion de formik para acceder a sus props, para desplegar mensajes de error (LoginFormik)

--- src/components/pure/forms/LoginFormik.jsx
+++ src/components/pure/forms/LoginFormik.jsx
@@ -1,7 +1,18 @@
 import { Formik, Form, Field } from 'formik';
-import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+
+/** Define esquema de analisis para la validacion valores */
+const loginSchema = Yup.object().shape({
+	email: Yup.string()
+			.email( 'Invalid email format!' )
+			.required( 'Email is required!' ),
+	passwd: Yup.string()
+			.required( 'Password is required!' )
+});
 
 
+
+/** Functional Component */
 const LoginFormik = () => {
 
 	const initialCredentials = {
@@ -14,40 +25,64 @@ const LoginFormik = () => {
 			<h4>Login Formik</h4>
 			<Formik
 				initialValues={ initialCredentials }
+				validationSchema={ loginSchema }
 				onSubmit={ async ( values ) => {
-					await new Promise( ( response ) => setTimeout( response, 500 ) );
+					await new Promise( ( response ) => setTimeout( response, 2000 ) );
 					alert( JSON.stringify( values, null, 4 ) );
+					localStorage.setItem( 'credentials', values )
 				} }
 			>
-				<Form>
 
-					<label htmlFor="email">Email</label>
-					<Field
-						id="email"
-						type="email"
-						name="email"
-						placeholder="example@email.co"
-					/>
+				{/** We obtein props from Formik */}
+				{
+					props => {
+						const {
+							values,			/** Valores del formulario */
+							touched,    	/** Campos que han sido tocados */
+							errors,      	/** Errores del formulario */
+							isSubmitting,	/** Indica si se estan enviando datos del formulario */
+							handleChange,   /** Cuando hay un cambio en un campo */
+							handleBlur, 	/** Cuando hay un cambio de foco */
+							handleSubmit, 	/** Cuando enviamos datos del formulario */
+						} = props;
 
-					<label htmlFor="email">Password</label>
-					<Field
-						id="password"
-						type="password"
-						name="passwd"
-						placeholder="Password"
-					/>
+						return (
+							<Form>
+								<label htmlFor="email">Email</label>
+								<Field
+									id="email"
+									type="email"
+									name="email"
+									placeholder="example@email.co"
+								/>
+								{	errors.email && touched.email && <div className="error">
+										<p>{ errors.email }</p>
+									</div>
+								}
 
-					<button type="submit">Login</button>
+								<label htmlFor="email">Password</label>
+								<Field
+									id="password"
+									type="password"
+									name="passwd"
+									placeholder="Password"
+								/>
+								{	errors.passwd && touched.passwd && <div className="error">
+										<p>{ errors.passwd }</p>
+									</div>
+								}
 
-				</Form>
+								<button type="submit">Login</button>
+								{	isSubmitting ? <p>Login you credentials</p> : null }
+
+							</Form>
+						)
+					}
+				}
+
 			</Formik>
 		</>
 	);
-};
-
-
-LoginFormik.propTypes = {
-
 };
 
 
