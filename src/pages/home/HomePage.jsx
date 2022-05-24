5a96fc5f7d556142c16d877e82f298c1a79dd231
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-05-24 08:51:34 -0500

    [+] Component: Define (NotFoundPage, HomePage)

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <>
+            <h1>Home Page</h1>
+            <h2>Dashboard</h2>
+        </>
+    );
+}
+
+export default HomePage;
