67e277ea825a359eb71337fa0404c8b23a8bdf8a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-16 11:18:42 -0500

    [+] Promises: Resolver funcion asincrona que retorna una promesa como promesa usando then/catch

--- src/components/PromisesComponent.jsx
+++ src/components/PromisesComponent.jsx
@@ -12,15 +12,34 @@ const PromisesComponent = () => {
             .finally(() => alert( `Successfully completed` ) );
     }
 
+    // Funcion asincrona que retorna una promesa
+    async function generatePromiseNumber(){
+        return Promise.resolve( 2 );
+    }
+    function obtainPromiseNumber(){
+        generatePromiseNumber()
+            .then( ( response ) => alert( `Response: ${ response }`) )
+            .catch( ( error ) => alert( `Something went wrong: ${ error }` ) )
+            .finally(() => alert( `Successfully completed` ) );
+    }
+
+
+
     return (
         <div>
             <h1>Promises <span>(Ejemplos)</span></h1>
             <div className="buttons">
                 <div>
-                    <p>Resolver funcion Asincrona</p>
+                    <p>Resolver funcion Asincrona que retorna un valor</p>
                     <button
                         onClick={ obtainNumber }
-                    >Obtener numero</button>
+                    >Obtener valor</button>
+                </div>
+                <div>
+                    <p>Resolver funcion Asincrona que retorna una promesa</p>
+                    <button
+                        onClick={ obtainPromiseNumber }
+                    >Obtener valor de una promesa</button>
                 </div>
             </div>
         </div>
