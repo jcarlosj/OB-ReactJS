163641aaa911b5ce53f7504658fd1813c4015114
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-16 17:16:18 -0500

    [$] Observables: Obtener numeros a traves de la creacion y subscripcion de un observable (next,error, complete)

--- src/components/ObservableComponent.jsx
+++ src/components/ObservableComponent.jsx
@@ -7,10 +7,22 @@ const ObservableComponent = () => {
 
     const newNumber = () => {
         // Subscricion al observable para obtener los datos
-        getNumbers.subscribe( number => {
-            console.log( number );
-            setNumber( number )
+        getNumbers.subscribe({
+            next( newNumber ) {     // Hace las veces de then
+                console.log( 'New number: ', newNumber );
+                setNumber( newNumber );
+            },
+            error( err ) {          // Hace las veces de catch
+                alert( `Something went wrong: ${ err }`);
+                console.error( `Something went wrong: ${ err }`);
+            },
+            complete() {            // Hace las veces de finally
+                alert( `Finished with: ${ number }` );
+                console.info( `Done with the observable!` );
+            }
         });
+
+        console.log( number );
     }
 
     return (
