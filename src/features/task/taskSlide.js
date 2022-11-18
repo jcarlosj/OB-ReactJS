87aa725160f4e0e202ffaafbfead68b42df6fad1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-11-18 15:28:56 -0500

    [+] RTK (TaskSlice): Casos add, toggle, remove & update

--- src/features/task/taskSlide.js
+++ src/features/task/taskSlide.js
@@ -7,11 +7,44 @@ const initialState = [];
 export const taskSlice = createSlice({
     name: 'task',
     initialState,
-    reducers: {}
+    reducers: {
+        // 5. Define casos del reducer
+        add: ( state, action ) => {
+            state.push( action.payload );
+        },
+        toggle: ( state, action ) => {
+            const id = action.payload;
+
+            state = state.map( ( task ) => {
+                if( task.id === id )
+                    task.completed = ! task.completed;
+            
+                return task;
+            });
+        },
+        remove: ( state, action ) => {
+            const id = action.payload;
+
+            const indexOfObject = state.findIndex( task => {
+                return task.id === id;
+            });
+
+            state.splice( indexOfObject, 1 );
+        },
+        update: ( state, action ) => {
+            const { id, data } = action.payload;
+
+            const indexOfObject = state.findIndex( task => {
+                return task.id === id;
+            });
+
+            state[ indexOfObject ] = { id, ...data };
+        }
+    }
 });
 
 
 // Los creadores de acciones se generan para cada función de reducción de casos.
-// export const {} = taskSlice.actions;
+export const { add, toggle, remove, update } = taskSlice.actions;
 
 export default taskSlice.reducer
